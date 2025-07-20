ecac310f53c88e5601c03d0e888c6e94b9818117
Sathish8248 <146933093+Sathish8248@users.noreply.github.com>
2025-07-20 13:01:14 +0530

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-function showInfo() {
-  const info = `
-    Cloud computing is the delivery of computing services like storage,
-    servers, databases, networking, and software over the internet.
-    It allows for flexible resources and faster innovation.
-  `;
-  const box = document.getElementById("infoBox");
-  box.innerText = info;
-  box.style.display = "block";
+function showMessage() {
+  const msg = document.getElementById("message");
+  msg.classList.remove("hidden");
 }
